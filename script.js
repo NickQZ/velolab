e8df64c509f7e6a899f095654d87a9eaaf046cdf
Nick <148161300+NickQZ@users.noreply.github.com>
2024-11-01 12:06:28 +1300

    Add files via upload
    Velolab Version 5.3  DATE: 1.11.2024
    FIXED REMAINING PAGES THAT DIDN'T RESPOND WELL TO MOBILE SCREENS, AND ADDED COPYRIGHT TO FOOTER + STYLING

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function toggleMenu() {
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript is working!');
+
+    // Toggle Menu
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
+    document.querySelector('.hamburger-menu').addEventListener('click', () => {
+        menu.classList.toggle("open");
+        icon.classList.toggle("open");
+    });
 
-
-// Auto play video
-document.addEventListener('DOMContentLoaded', function() {
+    // Back to Top Button
     const button = document.getElementById('back-to-top');
-
     window.onscroll = function() {
         if (window.scrollY > 200) {
             button.classList.remove('hidden');
@@ -20,24 +21,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     };
 
+    // Email Button
+    const mailtoButton = document.getElementById('email');
+    mailtoButton.addEventListener('click', function() {
+        const email = 'enquiries@velolab.co.nz';
+        const subject = encodeURIComponent('Velolab Inquiry');
+        const body = encodeURIComponent('I would like to inquire about...'); 
+        const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
+        window.location.href = mailtoLink;
+    });
 
-// Embed email
-const mailtoButton = document.getElementById('email');
-
-mailtoButton.addEventListener('click', function() {
-    const email = 'enquiries@velolab.co.nz';
-    const subject = encodeURIComponent('Velolab Inquiry');
-    const body = encodeURIComponent('I would like to inquire about...'); 
-
-    const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
-
-    window.location.href = mailtoLink;
-});
-
-    // Smooth scroll back to top of page
+    // Smooth Scroll Back to Top
     button.onclick = function(event) {
         event.preventDefault();
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
-
 });
